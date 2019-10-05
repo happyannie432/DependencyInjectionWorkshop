9c456b5f01b6b5bd2018fc1c44a8c10c74bd0dcd
Annie <annie.tang@titansoft.com.sg>
2019-10-05 11:02:59 +0800

    create feature

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,122 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+using SlackAPI;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
+    {
+        public bool Verify(string accountId, string inputPassword, string otp)
+        {
+            if (GetIsAccountLocked(accountId))
+            {
+                throw new FailedTooManyTimesException();
+            }
+
+            var password = GetPasswordFromDb(accountId);
+            var hashPassword = GetHashPassword(inputPassword);
+            var currentOpt = CurrentOpt(accountId);
+
+            if (hashPassword != password || currentOpt != otp)
+            {
+                NewMethod(accountId);
+                AddFailedCount(accountId);
+                Notify("Login Failed");
+
+                return false;
+            }
+
+            ResetFailedCount(accountId);
+            return true;
+        }
+
+        private static string CurrentOpt(string accountId)
+        {
+            var response = new HttpClient() {BaseAddress = new Uri("http://joey.com/")}.PostAsJsonAsync("api/otps", accountId)
+                .Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            return response.Content.ReadAsAsync<string>().Result;
+        }
+
+
+        //after extract it show static means there's no dependency with current instance (class)
+        private static bool GetIsAccountLocked(string accountId)
+        {
+            var isLockedResponse = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
+
+            isLockedResponse.EnsureSuccessStatusCode();
+            var isAccountLocked = isLockedResponse.Content.ReadAsAsync<bool>().Result;
+            return isAccountLocked;
+        }
+
+        private static void ResetFailedCount(string accountId)
+        {
+            var resetResponse = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
+            resetResponse.EnsureSuccessStatusCode();
+        }
+
+        private static void Notify(string message)
+        {
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
+        }
+
+        private static void AddFailedCount(string accountId)
+        {
+            var failedCountResponse =
+                new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
+
+            failedCountResponse.EnsureSuccessStatusCode();
+
+            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info($"accountId:{accountId} failed times:{failedCount}");
+        }
+
+        private static void NewMethod(string accountId)
+        {
+            var addFailedCountResponse = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
+            addFailedCountResponse.EnsureSuccessStatusCode();
+        }
+
+        private static string GetHashPassword(string inputPassword)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(inputPassword));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            var hashPassword = hash.ToString();
+            return hashPassword;
+        }
+
+        //extract class on this
+        private static string GetPasswordFromDb(string accountId)
+        {
+            string password;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                password = connection.Query<string>("spGetUserPassword", new {Id = accountId},
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            return password;
+        }
+    }
+
+    public class FailedTooManyTimesException : Exception
     {
     }
 }
